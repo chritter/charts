c1bfb84a789f9150fa754fcf275c83c978ef9f03
chritter <christian.h.ritter@gmail.com>
2021-09-23 09:07:14 -0400

    update postgres setup

--- stable/postgres/install_postgres.sh
+++
@@ -1,2 +0,0 @@
-  519  helm repo add bitnami https://charts.bitnami.com/bitnami
-  520  helm install postgres bitnami/postgresql
--- stable/postgres/setup.sh
+++
@@ -1,4 +0,0 @@
-export POSTGRES_PASSWORD=$(kubectl get secret --namespace minio postgres-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
-echo postgres password, $POSTGRES_PASSWORD
-
-
